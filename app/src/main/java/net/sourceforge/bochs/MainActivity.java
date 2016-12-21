60dbfd97afa7dbebb4caa5c10f488068ce2b86af
lubomyr <lubomyr31@gmail.com>
2016-12-21 23:58:30 +0200

    implemented verify and request storage permission

--- app/src/main/java/net/sourceforge/bochs/MainActivity.java
+++ app/src/main/java/net/sourceforge/bochs/MainActivity.java
@@ -1,11 +1,15 @@
 package net.sourceforge.bochs;
 
+import android.Manifest;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
@@ -25,6 +29,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
     private String configPath;
     private ViewPager viewPager;
     private ActionBar actionBar;
+    private final int REQUEST_EXTERNAL_STORAGE = 1;
 
     // Tab titles
     private String[] tabs = {"Storage", "Hardware", "Misc"};
@@ -38,8 +43,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
                 "/Android/data/" + getPackageName() + "/files/";
         configPath = appPath + "bochsrc.txt";
 
-        createDirIfNotExists();
-        checkConfig();
+        verifyStoragePermissions();
 
         // Initilization
         viewPager = (ViewPager) findViewById(R.id.pager);
@@ -158,5 +162,39 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         }
         return ret;
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE:
+                if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted
+                    createDirIfNotExists();
+                    checkConfig();
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public void verifyStoragePermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+            String[] PERMISSIONS_STORAGE = new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE };
+
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+            return;
+        }
+        createDirIfNotExists();
+        checkConfig();
+    }
+
 
 }
